52769daa9203d8cf55e9dacf6ab062d8b63286f2
eaydgdu <emre4415@gmail.com>
2018-03-23 16:04:26 +0100

    compass

---
+++ app/src/main/java/com/eaydgdu/evatvakit/CompassActivity.java
@@ -0,0 +1,54 @@
+package com.eaydgdu.evatvakit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.util.Log;
+
+import com.eaydgdu.evatvakit.View.Compass;
+
+
+public class CompassActivity extends AppCompatActivity {
+
+    private static final String TAG = "CompassActivity";
+
+    private Compass compass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+
+        compass = new Compass(this);
+        compass.arrowView = findViewById(R.id.main_image_hands);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "start compass");
+        compass.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        compass.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        compass.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "stop compass");
+        compass.stop();
+    }
+
+}
